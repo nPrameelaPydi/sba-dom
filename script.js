4d97ba0b91de5e0b85cd92264390c3d897f61b76
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-02 16:39:51 -0700

    caching elements

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //console.log(`SBA - dom manipulation`);
+
+//caching elements
+const taskForm = document.getElementById('taskForm');
+const taskInput = documnet.getElementById('taskInput');
+const taskList = document.querySelector('#taskList');
