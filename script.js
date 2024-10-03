fb61685d9f71e161090cb2923d4c3635f8cebbbb
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-02 18:04:08 -0700

    added Document Fragment & applied style to list element

--- script.js
+++ script.js
@@ -11,22 +11,35 @@ const listItem = document.createElement('li');
 
 function addTask() {
     event.preventDefault();
+
+    // Validating input
+    if (!taskInput.value.trim()) {
+        alert('Task cannot be empty');
+        return;
+    }
+
     //read and cache the input element
     const taskInputValue = taskInput.value;
-
     //create li item and add the taskInputValue to it
     const newLi = document.createElement('li');
     newLi.textContent = taskInputValue;
 
-    //append to ul
-    taskList.appendChild(newLi);
 
+    //using document fragment for performance optimization
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(newLi);
+    taskList.appendChild(fragment);
+
+
+
+    ////append to ul
+    //taskList.appendChild(newLi);
     //clear input value
     taskInput.value = '';
     //focus input element
     taskInput.focus();
 }
 
-
 taskForm.addEventListener('submit', addTask);
+
 
