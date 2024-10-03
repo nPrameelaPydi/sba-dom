6357c1081412f02a6819021c14d0fba6f6bedfbc
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-02 17:41:12 -0700

    add list item and add value to it

--- script.js
+++ script.js
@@ -4,11 +4,13 @@
 const taskForm = document.getElementById('taskForm');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.querySelector('#taskList');
+//const addTaskBtn = document.getElementById('addTask');
 
 //creating list items
 const listItem = document.createElement('li');
 
 function addTask() {
+    event.preventDefault();
     //read and cache the input element
     const taskInputValue = taskInput.value;
 
@@ -19,9 +21,12 @@ function addTask() {
     //append to ul
     taskList.appendChild(newLi);
 
-
+    //clear input value
+    taskInput.value = '';
+    //focus input element
+    taskInput.focus();
 }
 
 
-
+taskForm.addEventListener('submit', addTask);
 
