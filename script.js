c956abbcbde51193b0f1738154abfd26c23d5ea2
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-02 17:28:54 -0700

    modified js

--- script.js
+++ script.js
@@ -2,5 +2,26 @@
 
 //caching elements
 const taskForm = document.getElementById('taskForm');
-const taskInput = documnet.getElementById('taskInput');
+const taskInput = document.getElementById('taskInput');
 const taskList = document.querySelector('#taskList');
+
+//creating list items
+const listItem = document.createElement('li');
+
+function addTask() {
+    //read and cache the input element
+    const taskInputValue = taskInput.value;
+
+    //create li item and add the taskInputValue to it
+    const newLi = document.createElement('li');
+    newLi.textContent = taskInputValue;
+
+    //append to ul
+    taskList.appendChild(newLi);
+
+
+}
+
+
+
+
